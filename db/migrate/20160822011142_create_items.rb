e2168a25ff20b25cb213dd5a06009ae724894750
Vitaly Ivanov <universallly@gmail.com>
2016-08-21 23:49:39 -0400

    Items model created Migration of the model made Schema.rb changed 1st item created using rails console

--- db/migrate/20160822011142_create_items.rb
+++ db/migrate/20160822011142_create_items.rb
@@ -1,7 +1,11 @@
 class CreateItems < ActiveRecord::Migration[5.0]
   def change
     create_table :items do |t|
-
+        t.float   :price
+        t.string  :name
+        t.boolean :real
+        t.float   :weight
+        t.string :description
       t.timestamps
     end
   end
